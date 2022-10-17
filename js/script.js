23551769ce424eb636a5fd5d5c8c53234f81654a
MyUsername <alberto.baglivi@gmail.com>
2022-10-17 17:31:54 +0200

    add Bonus click

--- js/script.js
+++ js/script.js
@@ -7,6 +7,7 @@ btn.addEventListener("click" , CreateCards)
 
 
 
+
 // SECTION DEFINITION FUNCTION 
 
 
@@ -19,16 +20,27 @@ function ShowNumber(){
 
 function CreateCards(){
 
-let i=1
+const sel = document.getElementById("select_cards");   
+let i= 1;
+
 const arrayCards = []
 
-while(i<=100){
+while(i<=sel.value){
 
 let card = document.createElement('div');
-card.classList.add("card_grid");
+
+if(sel.value === "100"){
+    card.classList.add("card_grid");
+}
+if(sel.value === "81"){
+    card.classList.add("card_grid_9");
+}
+if(sel.value === "49"){
+    card.classList.add("card_grid_7");
+}
+
 card.innerHTML = i;
 card.addEventListener('click' , ShowNumber);
-
 
 grid.appendChild(card);
 arrayCards.push(card.innerHTML);
